c785b39f1341da1483162481f7ad89f4809a1b6a
DjokharMac <djokhar05@icloud.com>
2021-02-08 19:23:26 +0100

    Switched to Mac OS to setup for ios development

--- android/app/src/main/java/com/bringithome/MainApplication.java
+++ android/app/src/main/java/com/bringithome/MainApplication.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.agontuk.RNFusedLocation.RNFusedLocationPackage;
+import com.agontuk.RNFusedLocation.RNFusedLocationPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
